ecabddcc4bc68f83bdc5c242186a85cbb6da2458
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2026-06-16 19:21:02 -0300

    Add files via upload

---
+++ Bro Code JavaScript Course 2.0/index.js
@@ -0,0 +1,41 @@
+/*  ===== VARIÁVEIS =====
+
+let age = 25;
+
+console.log(`Your are ${age} years old`);
+
+let price = 24;
+
+console.log(`The Açaí price is ${price} R$`);
+
+let bestMusic = "Billie Jean";
+
+console.log(`Here some good record: ${bestMusic}`);
+
+let chuva = true;
+
+console.log(`Tempo chuvoso: ${chuva}`);
+
+// Exibir o tipo de valor de uma string:
+
+console.log(typeof age);
+
+console.log(typeof bestMusic);
+
+console.log(tyoeof ,chuva);
+
+*/
+
+let player = `Pomni`;
+
+let age = 25;
+
+let playerInAdventure = true;
+
+// Atribuir o conteúdo de uma variável JS em uma tag HTML
+
+document.getElementById("P1").textContent = `Player selected: ${player}`;
+
+document.getElementById("P2").textContent = `Player age: ${age}`;
+
+document.getElementById("P3").textContent = `Is the player in adventure?: ${playerInAdventure}`;
---
+++ Bro Code JavaScript Course 3.0/index.js
@@ -0,0 +1,19 @@
+let jogadores = 456;
+
+// Adição: jogadores = jogadores +1;
+
+// Subtração: jogadores = jogadores - 1;
+
+// Multiplicação: jogadores = jogadores * 2;
+
+// Divisão: jogadores = jogadores / 2;
+
+// Exponenciação: jogadores = jogadores ** 2;
+
+// Módulo de uma Divisão (Resto): let jogadoresExtras = jogadores % 2;
+
+// Adicionar "1" a uma variável: jogadores++;
+
+// Subtrair "1" a uma variável: jogadores--;
+
+console.log(jogadores);
