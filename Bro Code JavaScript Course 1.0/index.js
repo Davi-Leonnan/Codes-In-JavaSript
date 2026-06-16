b99495c35728da514ac395ae25a8baaf145b3cf3
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2026-06-16 15:24:29 -0300

    Add files via upload

---
+++ Bro Code JavaScript Course 1.0/index.js
@@ -0,0 +1,21 @@
+
+/*
+console.log(`Hello!`);
+console.log(`I like Açaí!`);
+*/
+
+/*
+
+// Oferece uma janela de texto toda vez que a página é carregada.
+
+window.alert(`Isso é um alerta!`);
+
+*/
+
+// "document.getElementById": Adiciona texto em uma tag HTML com um "id" específico.
+
+document.getElementById("H1").textContent = `Cadárpio`;
+document.getElementById("Hamburguer").textContent = `Hamburguer`;
+document.getElementById("French Fries").textContent = `French Fries`;
+document.getElementById("Hot-Dog").textContent = `Hot Dog`;
+
